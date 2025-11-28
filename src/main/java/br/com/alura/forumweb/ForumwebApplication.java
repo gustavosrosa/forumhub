6269c1c95070c698c8fb3a1655a5e5b3dde700ce
Gustavo Rosa <g.dsantosrosa@gmail.com>
2025-11-28 15:44:07 -0300

    FEAT: Adding initial structure

---
+++ src/main/java/br/com/alura/forumweb/ForumwebApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.forumweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumwebApplication.class, args);
+	}
+
+}
